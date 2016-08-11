925c602435692807ec3d80a9af380276dd6ed3e0
bha <bharathi0389@gmail.com>
2016-08-11 13:29:40 +0530

    changes done by dev2

--- calculator/src/main/java/com/calc/calc.java
+++ calculator/src/main/java/com/calc/calc.java
@@ -2,16 +2,19 @@ package com.calc;
 
 public class calc {
 
-	public int add(int a,int b) {
+	public double add(int a,int b) {
 		double result=a+b;
 		return result;
 	}
-}
-
 
 public double sub(double a, double b) {
 	double result = a - b;
 	return result;
+}
+
+public double mul(double a, double b) {
+	 double result = a * b;
+	 return result;
 }
 
 }
